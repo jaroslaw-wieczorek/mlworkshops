38827692bd790b123e572b52353b7795ffb3d294
Jarosław Wieczorek <jwieczorek@man.poznan.pl>
2020-04-03 18:48:14 +0200

    Return results.

--- skrypt.sh
+++ skrypt.sh
@@ -8,8 +8,9 @@ cut -f3 wikiniews_results.tsv > reference.txt
 awk 'BEGIN{FS=OFS="\t"}{print $0,"(sp1_"NR")"}' < hypothesis.txt > hypothesis.trn
 awk 'BEGIN{FS=OFS="\t"}{print $0,"(sp1_"NR")"}' < reference.txt > reference.trn
 
-# RUN SCLITE 
-sclite -f 0 -r reference.trn trn -h hypothesis.trn trn -e utf-8 -i rm -o all stdout
+# RUN SCLITE and return Scores
+sclite -f 0 -r reference.trn trn -h hypothesis.trn trn -e utf-8 -i rm -o all stdout | grep 'Scores' > results
+
 
 # Show files
 ls -l
