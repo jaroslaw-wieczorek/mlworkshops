5202be382f46690c312953c5f71b1772bdb2ae62
Jarosław Wieczorek <jwieczorek@man.poznan.pl>
2020-04-03 19:55:53 +0200

    Other data.

--- skrypt.sh
+++ skrypt.sh
@@ -11,6 +11,7 @@ awk 'BEGIN{FS=OFS="\t"}{print $0,"(sp1_"NR")"}' < reference.txt > reference.trn
 # RUN SCLITE and return Scores
 sclite -f 0 -r reference.trn trn -h hypothesis.trn trn -e utf-8 -i rm -o all stdout | grep 'Scores' > results
 
+sclite -f 0 -r reference.trn trn -h hypothesis.trn trn -e utf-8 -i rm -o pra stdout | grep "Scores:" | sed 's/Scores: (#C #S #D #I) // ' | awk '{ print ($2+$3+$4)/($1+$2+$3)}' | tail -n 10
 # Show last 10 lines 
 echo 'Results:'
 cat results | tail -n 10
@@ -19,7 +20,10 @@ echo '\n'
 
 cat results | awk 'BEGIN{OFS="\t"}{print $6, $7, $8, $9}' | awk '{ print ($2 + $3 + $4)/($2 + $3 + $1) }' | tail -n 10
 
+# Save to wer.tsv
+cat results | awk 'BEGIN{OFS="\t"}{print $6, $7, $8, $9}' | awk '{ print ($2 + $3 + $4)/($2 + $3 + $1) }' > wer.tsv
 # Show files
+echo '\n'
 echo 'Files:'
 ls
 echo '\n'
