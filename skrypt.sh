d84980b9dd014f637259ca8e19ad6a151d6db1bd
Jarosław Wieczorek <jwieczorek@man.poznan.pl>
2020-04-03 19:28:29 +0200

    Parse results.

--- skrypt.sh
+++ skrypt.sh
@@ -11,7 +11,12 @@ awk 'BEGIN{FS=OFS="\t"}{print $0,"(sp1_"NR")"}' < reference.txt > reference.trn
 # RUN SCLITE and return Scores
 sclite -f 0 -r reference.trn trn -h hypothesis.trn trn -e utf-8 -i rm -o all stdout | grep 'Scores' > results
 
+# Show last 10 lines 
 cat results | tail -n 10
+# Scores: (#C #S #D #I) 19 0 0 0 ...
+
+cat results | awk 'BEGIN{OFS="\t"}{print $6, $7, $8, $9}' | awk '{ print ($2 + $3 + $4)/($2 + $3 + $1) }' | tail -n 10
+
 
 # Show files
 ls -l
