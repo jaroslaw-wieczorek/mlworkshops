e7e9779059d99916a1e3b221e1b1ee44fb2bc34f
Jarosław Wieczorek <jwieczorek@man.poznan.pl>
2020-04-03 19:36:04 +0200

    New lines.

--- skrypt.sh
+++ skrypt.sh
@@ -1,5 +1,5 @@
 wc -l wikiniews_results.tsv > liczba_wierszy.txt
-
+echo '\n'
 # Split lines between hypothesis and reference 
 cut -f2 wikiniews_results.tsv > hypothesis.txt
 cut -f3 wikiniews_results.tsv > reference.txt
@@ -12,13 +12,17 @@ awk 'BEGIN{FS=OFS="\t"}{print $0,"(sp1_"NR")"}' < reference.txt > reference.trn
 sclite -f 0 -r reference.trn trn -h hypothesis.trn trn -e utf-8 -i rm -o all stdout | grep 'Scores' > results
 
 # Show last 10 lines 
+echo 'Results:'
 cat results | tail -n 10
 # Scores: (#C #S #D #I) 19 0 0 0 ...
+echo '\n'
 
 cat results | awk 'BEGIN{OFS="\t"}{print $6, $7, $8, $9}' | awk '{ print ($2 + $3 + $4)/($2 + $3 + $1) }' | tail -n 10
 
 # Show files
+echo 'Files:'
 ls
+echo '\n'
 
 
 
