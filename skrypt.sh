c4a44ccf2ca4a0002cf163138b40ef79489be1ad
Jarosław Wieczorek <jwieczorek@man.poznan.pl>
2020-04-03 18:35:14 +0200

    Debug tails.

--- skrypt.sh
+++ skrypt.sh
@@ -1,13 +1,17 @@
 wc -l wikiniews_results.tsv > liczba_wierszy.txt
 
 # Split lines between hypothesis and reference 
-cut -f3 wikiniews_results.tsv > reference.txt
 cut -f2 wikiniews_results.tsv > hypothesis.txt
+cut -f3 wikiniews_results.tsv > reference.txt
+
+echo "[*] Debug tails 1:"
+tail -n 3 hypothesis.txt
+tail -n 3 reference.txt
 
 
 # Crate TRN files
-awk 'BEGIN{FS=OFS="\t"}{print $0,"(sp1_"NR")"}' < reference.txt > reference.trn 
 awk 'BEGIN{FS=OFS="\t"}{print $0,"(sp1_"NR")"}' < hypothesis.txt > hypothesis.trn
+awk 'BEGIN{FS=OFS="\t"}{print $0,"(sp1_"NR")"}' < reference.txt > reference.trn 
 
 # Show files
 ls -l
@@ -15,11 +19,12 @@ ls -l
 # RUN SCLITE 
 sclite -f 0 -r reference.trn trn -h hypothesis.trn trn -e utf-8 -i rm -o all stdout
 
+echo "[*] Debug tails 2:"
 # wikiniews_results tail -n 10
-tail -n 10 wikiniews_results.trn
+tail -n 3 wikiniews_results.trn
 
 # reference tail -10
-tail -n 10 reference.trn
+tail -n 3 reference.trn
 
 # hypothesis tail 
-tail -n 10 hypothesis.trn
+tail -n 3 hypothesis.trn
