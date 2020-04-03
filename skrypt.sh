9905a7c83fd357c62d0e43672085351067b96b81
Jarosław Wieczorek <jwieczorek@man.poznan.pl>
2020-04-03 18:21:41 +0200

    Add sclite.

--- skrypt.sh
+++ skrypt.sh
@@ -1,3 +1,18 @@
 wc -l wikiniews_results.tsv > liczba_wierszy.txt
-awk 'BEGIN{FS=OFS="\t"}{print $0,"(sp1_"NR")"}' wikiniews_results.tsv > wikiniews_results.trn
+
+# Split lines between hypothesis and reference 
+cut -f3 wikiniews_results.tsv > reference.txt
+cut -f2 wikiniews_results.tsv > hypothesis.txt
+
+
+# Crate TRN files
+awk 'BEGIN{FS=OFS="\t"}{print $0,"(sp1_"NR")"}' < wikiniews_results.tsv >  wikiniews_results.trn
+
+awk 'BEGIN{FS=OFS="\t"}{print $0,"(sp1_"NR")"}' < reference.txt > reference.trn 
+awk 'BEGIN{FS=OFS="\t"}{print $0,"(sp1_"NR")"}' < hypothesis.txt > hypothesis.trn
+
+# Show files
 ls -l
+
+# RUN SCLITE 
+sclite -f 0 -r reference.trn trn -h hypothesis.trn trn -e utf-8 -i rm -o all stdout
